27416b6a7eed9be36193f2d5c8f909f8e0a44dcd
vncs <3456909+vncs1880@users.noreply.github.com>
2019-02-10 14:49:51 -0400

    please see MapInterface for what methods are missing

--- game-battle/src/main/java/org/game_battle/gameplay/Country.java
+++ game-battle/src/main/java/org/game_battle/gameplay/Country.java
@@ -3,11 +3,15 @@
  */
 package org.game_battle.gameplay;
 
+import java.util.List;
+
 /**
  * @author voda
  *
  */
 public class Country {
+
+	private int armies;
 
     /**
      *
@@ -15,5 +19,20 @@ public class Country {
     public Country() {
         // TODO Auto-generated constructor stub
     }
+
+	public void setArmyQty(int n_armies) {
+		armies = n_armies;
+	}
+
+	public int getArmies() {
+		return armies;
+	}
+
+	public List<Country> getNeighbours() {
+		// TODO Auto-generated method stub
+		return MapInterface.getNeighbours(this);
+	}
+
+
 
 }
--- game-battle/src/main/java/org/game_battle/gameplay/MapInterface.java
+++ game-battle/src/main/java/org/game_battle/gameplay/MapInterface.java
@@ -21,17 +21,22 @@ public class MapInterface {
 	}
 
 	public static List<Country> getCountries() {
-		// TODO Auto-generated method stub
+		// TODO return all territories
 		return Arrays.asList(new Country(),new Country(),new Country(),new Country());
 	}
 
 	public static List<Continent> getContinents() {
-		// TODO Auto-generated method stub
+		// TODO return all continents
 		return Arrays.asList(new Continent(10),new Continent(20),new Continent(30),new Continent(40));
 	}
 
 	public static List<Country> getCountriesByContinent(Continent continent) {
-		// TODO Auto-generated method stub
+		// TODO return all countries in a given continent
+		return null;
+	}
+
+	public static List<Country> getNeighbours(Country country) {
+		// TODO return all adjacent countries
 		return null;
 	}
 
