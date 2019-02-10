34d4bd8629ee8efe437409f2719ba12e7d72109c
mailgagannow@gmail.com <mailgagannow@gmail.com>
2019-02-09 21:02:17 -0500

    working with continents

--- game-battle/src/main/java/org/game_battle/gameplay/Continent.java
+++ game-battle/src/main/java/org/game_battle/gameplay/Continent.java
@@ -1,17 +1,41 @@
 package org.game_battle.gameplay;
 
+import java.util.ArrayList;
+
 public class Continent {
 
-	private int controlValue;
+    private int controlValue;
+    private String name;
+    private ArrayList<Country> countries;
 
-	public Continent(int ctrlValue) {
-		// TODO Auto-generated constructor stub
-		controlValue = ctrlValue;
-	}
+    public Continent(String name, int ctrlValue, ArrayList<Country> memberCountries) {
+        // TODO Auto-generated constructor stub
+        controlValue = ctrlValue;
+        this.name = name;
+        countries = memberCountries;
+        System.out.println("Continent Created: " + name + "Extra Armies: " + controlValue);
 
-	public int getControlValue() {
-		// TODO Auto-generated method stub
-		return controlValue;
-	}
+    }
+
+    /**
+     * Returns the control value associated with the continent
+     *
+     */
+    public int getControlValue() {
+        // TODO Auto-generated method stub
+        return controlValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns a list of the country objects that are on this continent
+     *
+     */
+    public ArrayList<Country> getMemberCountries() {
+        return countries;
+    }
 
 }
