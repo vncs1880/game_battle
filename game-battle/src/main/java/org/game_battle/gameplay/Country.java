5e2cebd84ce1a43a0426b5dd964018101631c079
mailgagannow@gmail.com <mailgagannow@gmail.com>
2019-02-10 14:05:09 -0500

    working with country

--- game-battle/src/main/java/org/game_battle/gameplay/Country.java
+++ game-battle/src/main/java/org/game_battle/gameplay/Country.java
@@ -1,9 +1,6 @@
-/**
- * 
- */
 package org.game_battle.gameplay;
 
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author voda
@@ -11,27 +8,54 @@ import java.util.List;
  */
 public class Country {
 
-	private int armies;
+    private int armies;
+    private String name;
+    private ArrayList<Country> neighbours;
+    private Player ruler;
+    private boolean hasRuler;
 
-	/**
-	 * 
-	 */
-	public Country() {
-		// TODO Auto-generated constructor stub
-	}
+    public Country(String name) {
+        // TODO Auto-generated constructor stub
+        this.name = name;
+        hasRuler = false;
+        armies = 0;
+        System.out.println("Country Created " + name);
+    }
 
-	public void setArmyQty(int resp) {
-		armies = resp;
-	}
+    public void setArmyQty(int num) {
+        armies = num;
+    }
 
-	public int getArmies() {
-		// TODO Auto-generated method stub
-		return armies;
-	}
+    public int getArmies() {
+        // TODO Auto-generated method stub
+        return armies;
+    }
 
-	public List<Country> getNeighbours() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public ArrayList<Country> getAdjacentNeighbours() {
+        // TODO Auto-generated method stub
+        return neighbours;
+    }
+
+    /**
+     * When a player occupies a country the player object is set as the ruler of
+     * the country
+     */
+    public void setRuler(Player ruler) {
+        hasRuler = true;
+        this.ruler = ruler;
+
+    }
+
+    /**
+     * Returns the player object who currently rules the country
+     *
+     */
+    public Player getRuler() {
+        return ruler;
+    }
+
+    public boolean hasRuler() {
+        return hasRuler;
+    }
 
 }
