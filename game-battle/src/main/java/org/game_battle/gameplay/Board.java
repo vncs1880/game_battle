5cff386e9e61598ca3da6ea389a2b80273ea8141
vncs <3456909+vncs1880@users.noreply.github.com>
2019-02-28 08:08:31 -0500

    patching

--- game-battle/src/main/java/org/game_battle/gameplay/Board.java
+++ game-battle/src/main/java/org/game_battle/gameplay/Board.java
@@ -98,10 +98,6 @@ public class Board {
 			Player player = players.get(k);
 			List<Country> countries = player.getCountries();
 			countries.add(c);
-
-			// countriesToDistribute.remove(c);
-			k = (++k == players.size()) ? 0 : k;
-
 			player.setPreviousCountriesQty(countries.size());
 			//countriesToDistribute.remove(c);
 			k = (++k==players.size())?0:k;
