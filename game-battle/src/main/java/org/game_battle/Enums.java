01623ef4be84f7d81a4e669df029caa6c6ebd31b
suseelareshma25 <47037560+suseelareshma25@users.noreply.github.com>
2019-03-01 18:57:03 -0500

    Enum File

---
+++ game-battle/src/main/java/org/game_battle/Enums.java
@@ -0,0 +1,14 @@
+package org.game_battle;
+
+public class Enums {
+	public static class MapView
+	{
+		public enum userAction
+		{
+			EDIT_CONTINENT_VALUE, ADD_NEIGHBOURS, REMOVE_NEIGHBOURS    ;
+		}
+	}
+
+}
+
+
