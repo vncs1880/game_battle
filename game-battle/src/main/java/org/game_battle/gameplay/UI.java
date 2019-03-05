5f77a32157107c0238cf9d6d65b1f0336c5a0a39
root <root@localhost.localdomain>
2019-03-05 17:14:34 -0500

    final-javadoc

--- game-battle/src/main/java/org/game_battle/gameplay/UI.java
+++ game-battle/src/main/java/org/game_battle/gameplay/UI.java
@@ -33,58 +33,57 @@ public class UI {
 		Country picked = selectCountry("Select one (MANDATORY)", "select country", countries);
 
 		System.out.println(picked);
-		
+
 		System.out.println(askNumber("Select one (MANDATORY)", "pick a number", 1, 12));
 		System.out.println(askNumber("Select one (MANDATORY)", "pick a number", 20, 55));
-		
+
 		System.out.println(isUserOk("Yes/No?", "test"));
-		
-		System.out.println(askText("",""));
+
+		System.out.println(askText("", ""));
 
 	}
-	
-	
 
 	public static String askText(String msg, String title) {
 		String text = null;
 		do {
-	        text = JOptionPane.showInputDialog(null, msg, title, JOptionPane.QUESTION_MESSAGE);
-		} while (text == null); 
+			// JFrame frame = new JFrame();
+
+			text = JOptionPane.showInputDialog(null, msg, title, JOptionPane.QUESTION_MESSAGE);
+		} while (text == null);
 		return text;
 	}
 
-
-
 	public static boolean isUserOk(String title, String prompt) {
-		int picked = JOptionPane.showConfirmDialog(null, prompt, title, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-		return (picked==JOptionPane.OK_OPTION)?true:false;
+		int picked = JOptionPane.showConfirmDialog(null, prompt, title, JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE);
+		return (picked == JOptionPane.OK_OPTION) ? true : false;
 	}
 
-	public static int askNumber(String title,  String prompt, int min, int max) {
+	public static int askNumber(String title, String prompt, int min, int max) {
 		List<Integer> collect = IntStream.rangeClosed(min, max).boxed().collect(Collectors.toList());
 		Integer[] options = (Integer[]) collect.toArray((new Integer[collect.size()]));
 		Integer picked = null;
 		do {
-			picked = (Integer) JOptionPane.showInputDialog(null, prompt
-					, title, JOptionPane.QUESTION_MESSAGE
-					, null, options, options[0]);
+			picked = (Integer) JOptionPane.showInputDialog(null, prompt, title, JOptionPane.QUESTION_MESSAGE, null,
+					options, options[0]);
 		} while (picked == null);
-		
+
 		return picked;
 	}
 
 	public static Country selectCountry(String title, String prompt, List<Country> countries) {
-		//MapInterface m = new MapInterface();
-		//String[] y = x.toArray(new String[0]);
+		// MapInterface m = new MapInterface();
+		// String[] y = x.toArray(new String[0]);
 		Country[] countries_array = countries.toArray(new Country[0]);
-		
+
 		Country picked;
 		do {
-			picked = (Country) JOptionPane.showInputDialog(null, prompt
-					, title, JOptionPane.QUESTION_MESSAGE
-					, null, countries_array, countries_array[0]);
+			// JFrame frame = new JFrame(); frame.toFront();
+
+			picked = (Country) JOptionPane.showInputDialog(null, prompt, title, JOptionPane.QUESTION_MESSAGE, null,
+					countries_array, countries_array[0]);
 		} while (picked == null);
-		
+
 		return picked;
 	}
 
