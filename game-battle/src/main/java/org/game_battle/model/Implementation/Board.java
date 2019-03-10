c04ff90b98a660be96a1767004e342c0ddbc4d2d
suseelareshma25 <47037560+suseelareshma25@users.noreply.github.com>
2019-03-10 16:19:05 -0400

    Demo Code

--- game-battle/src/main/java/org/game_battle/model/Implementation/Board.java
+++ game-battle/src/main/java/org/game_battle/model/Implementation/Board.java
@@ -60,7 +60,7 @@ public class Board {
 		//// generate a list of lists, each w random countries
 		mapinterface = new MapInterface();
 		LOG.info("\r\n\r\n<<UI prompt in the background. Please use ALT-TAB>>");// TODO make the UI come to foreground
-		int totalPlayers = UI.askNumber("Initializing board", "How many players?", 2, 25);
+		int totalPlayers = UI.askNumber("Initializing board", "How many players?", 2, 6);
 		LinkedList<Player> players = new LinkedList<Player>();
 		for (int i = 0; i < totalPlayers; i++) {
 			String name = "dummy player";
