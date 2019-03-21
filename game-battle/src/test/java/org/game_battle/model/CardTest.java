9580c9f3a2841f898219a14934e55a2109802fc4
mailgagannow <mailgagannow@gmail.com>
2019-03-21 14:35:32 -0400

    card test cases

--- game-battle/src/test/java/org/game_battle/model/CardTest.java
+++ game-battle/src/test/java/org/game_battle/model/CardTest.java
@@ -10,38 +10,46 @@ import junit.framework.Assert;
 public class CardTest {
 
 	Card ArtilleryCard, InfantryCard, CavalryCard;
-	
+
 	@Before
 	public void setup() {
 		ArtilleryCard = new Card(Card.Sort.ARTILLERY);
 		InfantryCard = new Card(Card.Sort.INFANTRY);
 		CavalryCard = new Card(Card.Sort.CAVALRY);
-		
+
 	}
-	
+
 	@Test
 	public void cardsNotNull() {
 		Assert.assertNotNull(ArtilleryCard);
 		Assert.assertNotNull(CavalryCard);
 		Assert.assertNotNull(InfantryCard);
 	}
-	
+
 	@Test
 	public void getType_CorrectCardValues() {
-		Assert.assertEquals(Card.Sort.ARTILLERY,ArtilleryCard.getType());
-		Assert.assertEquals(Card.Sort.CAVALRY,CavalryCard.getType());
-		Assert.assertEquals(Card.Sort.INFANTRY,InfantryCard.getType());
+		Assert.assertEquals(Card.Sort.ARTILLERY, ArtilleryCard.getType());
+		Assert.assertEquals(Card.Sort.CAVALRY, CavalryCard.getType());
+		Assert.assertEquals(Card.Sort.INFANTRY, InfantryCard.getType());
 	}
-	
+
 	@Test
 	public void toString_CorrectFormat() {
-		Assert.assertEquals("ARTILLERY",ArtilleryCard.toString());
-		Assert.assertEquals("CAVALRY",CavalryCard.toString());
-		Assert.assertEquals("INFANTRY",InfantryCard.toString());
+		Assert.assertEquals("ARTILLERY", ArtilleryCard.toString());
+		Assert.assertEquals("CAVALRY", CavalryCard.toString());
+		Assert.assertEquals("INFANTRY", InfantryCard.toString());
 	}
-	
-	
 
-	
-	
+	@Test
+	public void Sort_Enum_CorrectSize() {
+		Assert.assertEquals(3, Card.Sort.values().length);
+	}
+
+	@Test
+	public void Sort_Enum_NonEqualValues() {
+		Assert.assertFalse(Card.Sort.ARTILLERY.equals(Card.Sort.CAVALRY));
+		Assert.assertFalse(Card.Sort.CAVALRY.equals(Card.Sort.ARTILLERY));
+		Assert.assertFalse(Card.Sort.INFANTRY.equals(Card.Sort.ARTILLERY));
+	}
+
 }
