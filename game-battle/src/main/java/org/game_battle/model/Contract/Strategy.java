2af9edadfd59f0f24fa3576712381be3c0364762
basant singh <mr.singh.basant@gmail.com>
2019-03-30 17:22:47 -0400

    player commit

---
+++ game-battle/src/main/java/org/game_battle/model/Contract/Strategy.java
@@ -0,0 +1,15 @@
+package org.game_battle.model.Contract;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.game_battle.model.Implementation.Country;
+
+public interface Strategy {
+    int execute(int a, int b); 
+    String askText(String msg, String title);
+    int askNumber(String title,  String prompt, int min, int max);
+    public Collection getObjs(String prompt, Object[] objs);
+	public boolean isUserOk(String title, String prompt) ;
+	public  Country selectCountry(String title, String prompt, List<Country> countries) ;
+}
