45a541f846fe800ade996037b01f49bed6cc9a95
suseelareshma25 <47037560+suseelareshma25@users.noreply.github.com>
2019-04-06 17:43:59 -0400

    Random Strategy implemented

--- game-battle/src/main/java/org/game_battle/model/Implementation/AggresiveStrategyImpl.java
+++ game-battle/src/main/java/org/game_battle/model/Implementation/AggresiveStrategyImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JList;
 import javax.swing.JOptionPane;
@@ -13,7 +14,7 @@ import org.game_battle.model.Contract.Strategy;
 public class AggresiveStrategyImpl implements Strategy {
 
 	@Override
-	public int askNumber(String title, String prompt, int min, int max,int numOfCountries, boolean flag) {
+	public int askNumber(String title, String prompt, int min, int max, int numOfCountries, boolean flag) {
 		// TODO Auto-generated method stub
 		if (title.equals("Reinforcement phase") || title.equals("Attack phase")) {
 			return max;
@@ -25,19 +26,28 @@ public class AggresiveStrategyImpl implements Strategy {
 	}
 
 	@Override
+	/*
+	 * public Collection getObjs(String prompt, Object[] objs) { // TODO
+	 * Auto-generated method stub List list = Arrays.asList(objs); Collection
+	 * collection = new ArrayList(list); return collection;
+	 * 
+	 * }
+	 */
 	public Collection getObjs(String prompt, Object[] objs) {
 		// TODO Auto-generated method stub
-		List list = Arrays.asList(objs);
-		Collection collection = new ArrayList(list);
-		return  collection;
+		List list = new ArrayList(Arrays.asList(objs));
+		List responseList = getRandomElement(list, 3);
+		Collection collection = new ArrayList(responseList);
+		System.out.println("COLLECTIONNNNNNNNNNNNN" + collection);
+		return collection;
 
 	}
 
 	@Override
 	public boolean isUserOk(String title, String prompt) {
 		// TODO Auto-generated method stub
-			return true;
-		
+		return true;
+
 	}
 
 	@Override
@@ -81,16 +91,11 @@ public class AggresiveStrategyImpl implements Strategy {
 		return null;
 	}
 
-	
 	@Override
 	public int setArmies(int NumOfArmies) {
 		// TODO Auto-generated method stub
 		return NumOfArmies;
 	}
-
-	
-
-	
 
 	@Override
 	public Country[] doAttack(Country offendingCountry, Country deffendingCountry, int attackerDiceRoll,
@@ -101,6 +106,24 @@ public class AggresiveStrategyImpl implements Strategy {
 		cn[0] = winner;
 		cn[1] = loser;
 		return cn;
+	}
+
+	public List<Card> getRandomElement(List<Card> list, int totalItems) {
+		System.out.println("LISTTTTTTTTTTTTTTTTTTTTTTTT##############" + list);
+
+		Random rand = new Random();
+
+		List<Card> newList = new ArrayList<>();
+		for (int i = 0; i < totalItems; i++) {
+
+			int randomIndex = rand.nextInt(list.size());
+			System.out.println("LISTTTTTTTTTTTTTTTTTTTTTTTTSIZZZZZZZZZZZE##############" + randomIndex);
+
+			newList.add(list.get(randomIndex));
+
+			list.remove(randomIndex);
+		}
+		return newList;
 	}
 
 }
--- game-battle/src/main/java/org/game_battle/model/Implementation/BenevolentStrategyImpl.java
+++ game-battle/src/main/java/org/game_battle/model/Implementation/BenevolentStrategyImpl.java
@@ -1,7 +1,10 @@
 package org.game_battle.model.Implementation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 import org.game_battle.model.Contract.Strategy;
 
@@ -16,19 +19,25 @@ public class BenevolentStrategyImpl implements Strategy{
 	@Override
 	public int askNumber(String title, String prompt, int min, int max, int numOfCountries , boolean flag) {
 		// TODO Auto-generated method stub
+		//for(Country countries:)
 		return 0;
 	}
 
 	@Override
 	public Collection getObjs(String prompt, Object[] objs) {
 		// TODO Auto-generated method stub
-		return null;
+		List list = new ArrayList(Arrays.asList(objs));
+		List responseList=getRandomElement(list, 3);
+		Collection collection = new ArrayList(responseList);
+		System.out.println("COLLECTIONNNNNNNNNNNNN"+collection);
+		return collection;
+		
 	}
 
 	@Override
 	public boolean isUserOk(String title, String prompt) {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
@@ -42,7 +51,7 @@ public class BenevolentStrategyImpl implements Strategy{
 	@Override
 	public int setArmies(int NumOfArmies) {
 		// TODO Auto-generated method stub
-		return 0;
+		return NumOfArmies;
 	}
 
 	
@@ -52,6 +61,25 @@ public class BenevolentStrategyImpl implements Strategy{
 			int defenderDiceRoll) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public List<Card> getRandomElement(List<Card> list, int totalItems) {
+		System.out.println("LISTTTTTTTTTTTTTTTTTTTTTTTT##############"+list);
+
+		Random rand = new Random();
+
+		List<Card> newList = new ArrayList<>();
+		for (int i = 0; i < totalItems; i++) {
+
+			int randomIndex = rand.nextInt(list.size());
+			System.out.println("LISTTTTTTTTTTTTTTTTTTTTTTTTSIZZZZZZZZZZZE##############"+randomIndex);
+
+
+			newList.add(list.get(randomIndex));
+
+			list.remove(randomIndex);
+		}
+		return newList;
 	}
 	
 
